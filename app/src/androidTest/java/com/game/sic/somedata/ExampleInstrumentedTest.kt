65d89cd6f1c65e635946c568439cc67e711a8d75
gubatenko <gubatenko.aleksandr@gmail.com>
2018-09-12 22:29:10 +0300

    refactoring

--- app/src/androidTest/java/com/game/sic/somedata/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/game/sic/somedata/ExampleInstrumentedTest.kt
@@ -2,13 +2,11 @@ package com.game.sic.somedata
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import com.game.sic.somedata.repo.local.model.Post
 import com.game.sic.somedata.repo.Repo
-import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.concurrent.CountDownLatch
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
@@ -17,27 +15,15 @@ class ExampleInstrumentedTest {
     fun createDb() {
         val context = InstrumentationRegistry.getTargetContext()
         Repo.setup(context)
+        Repo.database.postDao().all().observeForever { println(it.toString()) }
     }
 
     @Test
     fun useAppContext() {
-        val signal = CountDownLatch(1)
-
-        async {
-            Repo.database.postDao().all().observeForever {
-                println(it.toString())
-            }
-
-            Repo.database.postDao().insert(Post())
-
+        runBlocking {
             val posts = Repo.service.posts().await()
-
             Repo.database.postDao().insert(posts)
-
-            signal.countDown()
-
         }
-
-        signal.await()
     }
+
 }
